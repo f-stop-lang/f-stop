32e3f2d4c44cc3a0cb5b8b0b240e4f006672f7ac
MrKomodoDragon <svrchn921@outlook.com>
2021-06-03 13:47:32 -0700

    first lexer

--- f-stop/lexer.py
+++ f-stop/lexer.py
@@ -1,2 +1,9 @@
 import rply
-import random
+lexer  = rply.LexerGenerator()
+lexer.add("OPEN", r"OPEN")
+lexer.add("AS", r"AS")
+lexer.add("STRING", r"\".+\"")
+lexer.ignore(r"/s+")
+l = lexer.build()
+for t in l.lex('OPEN "test.png" AS im'):
+	print(t)
