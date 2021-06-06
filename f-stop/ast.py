deac4f06612b4188d7c37d0e30f243c1a690254e
Jay3332 <40323796+jay3332@users.noreply.github.com>
2021-06-06 16:47:02 -0400

    Add some new classes and __reprs__

--- f-stop/ast.py
+++ f-stop/ast.py
@@ -2,8 +2,32 @@ class String:
     def __init__(self, value: str) -> None:
         self.value = value
     
+    def __repr__(self) -> str:
+        # Backwards compatibility for Python <3.5
+        return '<String "{}">'.format(self.value)
+    
     def eval(self, env) -> str:
         return self.value
+    
+    
+class Coordinate:
+    """
+    Equivalent of a tuple in Python.
+    """
+    def __init__(self, value: tuple) -> None:
+        self.value: tuple = value
+        
+    @property
+    def x(self) -> int:
+        return self.value[0]
+    
+    @property
+    def y(self) -> int:
+        return self.value[1]
+    
+    def __repr__(self) -> str: 
+        # Backwards compatibility for Python <3.5
+        return '<Coordinate {!r}>'.format(self.value)
     
     
 class Env:
