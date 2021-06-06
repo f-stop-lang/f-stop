e7b1345edc018281a2f87e8161606d04d6b354c4
Jay3332 <40323796+jay3332@users.noreply.github.com>
2021-06-06 15:28:58 -0400

    Improve code style
    SMH

--- f-stop/parser.py
+++ f-stop/parser.py
@@ -1,6 +1,10 @@
-from lark import Lark, 
-grammer = open('grammar.lark').read()
-f_stop_parser = Lark(grammer)
+from lark import Lark
+
+with open('grammar.lark') as grammar:
+    grammar = grammar.read()
+    
+f_stop_parser = Lark(grammar)
 text = 'OPEN "test.png" AS im'
 parsed = f_stop_parser.parse(text)
+
 print(parsed.pretty())
