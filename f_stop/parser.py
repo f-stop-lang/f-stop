2796c8e73baa7afe9996795f2c6154a169f2892a
Tom-the-Bomb <thello522@gmail.com>
2021-06-07 18:21:59 -0400

    sussy first commit

--- f_stop/parser.py
+++ f_stop/parser.py
@@ -1,17 +1,16 @@
 from lark import Lark, Transformer, v_args
-from transformer import FStopTransformer
-from fast import Env
-import PIL.Image
+from PIL import Image
 
+from .transformer import FStopTransformer
+from .fast import Env
 
-
-with open('grammar.lark') as grammar:
-    grammar = grammar.read()
+if __name__ == '__main__':
+    with open('grammar.lark') as grammar:
+        grammar = grammar.read()
     
-f_stop_parser = Lark.open('grammar.lark', rel_to=__file__)
-text = "OPEN 'test.png' AS im"
-parsed = f_stop_parser.parse(text)
-print(parsed.pretty())
-x = FStopTransformer().transform(parsed).eval(Env())
-
+    f_stop_parser = Lark.open('grammar.lark', rel_to=__file__)
+    text = "OPEN 'test.png' AS im"
+    parsed = f_stop_parser.parse(text)
+    print(parsed.pretty())
+    x = FStopTransformer().transform(parsed).eval(Env())
 
