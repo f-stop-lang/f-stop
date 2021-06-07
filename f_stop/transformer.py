58ebe0644d3d60c91883afd3cefc6e0e5d46f546
MrKomodoDragon <svrchn921@outlook.com>
2021-06-07 15:58:25 -0700

    Jay you is a dumbo yes yes

--- f_stop/transformer.py
+++ f_stop/transformer.py
@@ -5,9 +5,11 @@ from fast import String, Open  #type: ignore
 
 @v_args(inline=True)
 class FStopTransformer(Transformer):
-    def open_stmt(self, image: ..., name: str) -> None:
+    def open_stmt(self, image: str, name: str) -> None:
+        print(image)
+        print(name)
         return Open(image=image, value=name)
     
     def string(self, s: str) -> String:
-        val = str(s)[1:-1]
+        val = str(s).strip('"').strip("'")
         return String(s)
