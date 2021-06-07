105c4ef31afdcc474ad7816ca0bbb9c6d74065a9
Jay3332 <40323796+jay3332@users.noreply.github.com>
2021-06-07 18:50:18 -0400

    Follow PEP 8

--- f_stop/transformer.py
+++ f_stop/transformer.py
@@ -1,13 +1,13 @@
 from lark import Transformer, v_args
 
-from fast import String, Open #type: ignore
+from fast import String, Open  #type: ignore
 
 
 @v_args(inline=True)
 class FStopTransformer(Transformer):
-    def open_stmt(self, image, name):
+    def open_stmt(self, image: ..., name: str) -> None:
         return Open(image=image, value=name)
     
     def string(self, s: str) -> String:
-        val = str(s).strip('"').strip("'")
+        val = str(s)[1:-1]
         return String(s)
