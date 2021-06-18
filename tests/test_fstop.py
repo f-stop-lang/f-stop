182e29cd0e4773a1d146e3edb0acded9a9f60225
MrKomodoDragon <svrchn921@outlook.com>
2021-06-18 09:15:02 -0700

    Lalr for speed is the pog

--- tests/test_fstop.py
+++ tests/test_fstop.py
@@ -7,11 +7,12 @@ print(__name__)
 if __name__ == '__main__':
     with open("test.ft") as ft:
         text = ft.read()
-    f_stop_parser = Lark.open('../f_stop/grammar.lark', rel_to=__file__)
+    f_stop_parser = Lark.open('../f_stop/grammar.lark', rel_to=__file__, parser='lalr')
+    print(f_stop_parser)
     parsed = f_stop_parser.parse(text)
     print(parsed.pretty())
     env = Env()
     x = FStopTransformer().transform(parsed)
     x.eval(env)
     assert "im" in env.images
-    env['im'].show(title="thing.png")
+    env['im'].show("thing.png")
