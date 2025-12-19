4d17c1ce2385b0481b6ad130d4d318f8aea56654
MrKomodoDragon <svrchn921@outlook.com>
2025-12-19 15:36:22 -0800

    begin cleanup of code

---
+++ test_fstop.py
@@ -0,0 +1,15 @@
+from PIL import Image, ImageDraw
+from f_stop.fast import Env
+from f_stop.transformer import FStopTransformer
+from lark import Lark
+
+if __name__ == '__main__':
+    with open('test.fstop') as ft:
+        text = ft.read()
+    f_stop_parser = Lark.open('f_stop/grammar.lark', rel_to=__file__)
+    parsed = f_stop_parser.parse(text)
+    print(parsed.pretty())
+    env = Env()
+    x = FStopTransformer().transform(parsed)
+    x.eval(env)
+
